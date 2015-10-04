16ec2f0500b2d69a63bba4e2cf2e2556bd436619
Chrysostomus <mattitheman@gmail.com>
2015-10-04 17:04:08 +0800

    D move executables to folder /bin

---
+++ bin/volume_status.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+panel_volicon()
+{
+        volStatus=$(pulseaudio-ctl full-status | awk '{print $2}')
+        volLevel=$(pulseaudio-ctl C)
+
+        if [ "$volStatus" == "yes" ]
+                then echo $GREEN " ${volLevel}"
+        elif [ "$volStatus" == "no" ]
+                then echo $RED " ${volLevel}"
+	else echo " ?"
+        fi
+}
+
+echo "V"%{A:dvol:}%{A2:volume mute:}%{A5:volume down:}%{A4:volume up:}$(panel_volicon)%{A}%{A}%{A}%{A}> "$PANEL_FIFO"
