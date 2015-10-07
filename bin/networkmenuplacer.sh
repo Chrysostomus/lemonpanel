5f736f9dfe65c254dbd1400eb2f94cb025284ff5
Chrysostomus <mattitheman@gmail.com>
2015-10-07 16:41:11 +0000

    Update networkmenuplacer.sh
    Fix bug preventing functioning

--- bin/networkmenuplacer.sh
+++ bin/networkmenuplacer.sh
@@ -2,12 +2,14 @@
 #Used to edit config file of nmcli_dmenu to spawn 
 #menu where the pointer is located.
 eval $(xdotool getmouselocation --shell)
+menu_widht=500
 monitor_widht=$(xdpyinfo | awk -F'[ x]+' '/dimensions:/{print $3}')
 maxx=$(echo "$monitor_widht - $menu_widht" | bc)
 XP=$(echo "$X - 15" | bc)
 [[ $XP -gt $maxx ]] && XP=$maxx
 
-oldx=$(awk '/x/{print $3}' .config/networkmanager-dmenu/config.ini)
-sed -i 's/'$oldx'/'$XP'/g' .config/networkmanager-dmenu/config.ini
-oldy=$(awk '/y/{print $3}' .config/networkmanager-dmenu/config.ini)
-sed -i 's/'$oldy'/'$PANEL_HEIGHT'/g' .config/networkmanager-dmenu/config.ini
+oldx=$(awk '/x =/{print $3}' .config/networkmanager-dmenu/config.ini)
+sed -i '/x =/s/'$oldx'/'$XP'/g' .config/networkmanager-dmenu/config.ini
+echo $XP
+oldy=$(awk '/y =/{print $3}' .config/networkmanager-dmenu/config.ini)
+sed -i '/y =/s/'$oldy'/'$PANEL_HEIGHT'/g' .config/networkmanager-dmenu/config.ini
