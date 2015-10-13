3ea4441d0c7245897dfc26743e45499fa0fe5a45
Chrysostomus <mattitheman@gmail.com>
2015-10-13 22:31:13 +0700

    Update networkmenuplacer.sh

--- bin/networkmenuplacer.sh
+++ bin/networkmenuplacer.sh
@@ -8,8 +8,8 @@ maxx=$(echo "$monitor_widht - $menu_widht" | bc)
 XP=$(echo "$X - 15" | bc)
 [[ $XP -gt $maxx ]] && XP=$maxx
 
-oldx=$(awk '/x =/{print $3}' .config/networkmanager-dmenu/config.ini)
-sed -i '/x =/s/'$oldx'/'$XP'/g' .config/networkmanager-dmenu/config.ini
+oldx=$(awk '/x =/{print $3}' $HOME/.config/networkmanager-dmenu/config.ini)
+sed -i '/x =/s/'$oldx'/'$XP'/g' $HOME/.config/networkmanager-dmenu/config.ini
 echo $XP
-oldy=$(awk '/y =/{print $3}' .config/networkmanager-dmenu/config.ini)
-sed -i '/y =/s/'$oldy'/'$PANEL_HEIGHT'/g' .config/networkmanager-dmenu/config.ini
+oldy=$(awk '/y =/{print $3}' $HOME/.config/networkmanager-dmenu/config.ini)
+sed -i '/y =/s/'$oldy'/'$PANEL_HEIGHT'/g' $HOME/.config/networkmanager-dmenu/config.ini
