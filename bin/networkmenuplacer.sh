39b1c6ca28bba19a391abdd4117714e06548ef52
Chrysostomus <mattitheman@gmail.com>
2015-11-17 20:16:41 +0700

    Update networkmenuplacer.sh

--- bin/networkmenuplacer.sh
+++ bin/networkmenuplacer.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #Used to edit config file of nmcli_dmenu to spawn 
 #menu where the pointer is located.
 eval $(xdotool getmouselocation --shell)
@@ -6,7 +6,7 @@ menu_widht=500
 monitor_widht=$(wattr w $(lsw -r))
 maxx=$(( $monitor_widht - $menu_widht ))
 XP=$(( $X - 15 ))
-[[ $XP -gt $maxx ]] && XP=$maxx
+[ $XP -gt $maxx ] && XP=$maxx
 
 oldx=$(awk '/x =/{print $3}' $HOME/.config/networkmanager-dmenu/config.ini)
 sed -i '/x =/s/'$oldx'/'$XP'/g' $HOME/.config/networkmanager-dmenu/config.ini
