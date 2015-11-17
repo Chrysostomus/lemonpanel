49d31595aeb9465485946bd84cfbbb88725b178a
Chrysostomus <mattitheman@gmail.com>
2015-11-17 21:47:54 +0700

    Update volume_status.sh

--- bin/volume_status.sh
+++ bin/volume_status.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-export PANEL_FIFO="/tmp/panel-fifo"
 panel_volicon()
 {
         volStatus=$(pulseaudio-ctl full-status | awk '{print $2}')
@@ -12,5 +11,4 @@ panel_volicon()
 	else echo " ?"
         fi
 }
-[ -e /tmp/panel-fifo1 ] && echo "V"%{A:dvol:}%{A2:volume mute:}%{A5:volume down:}%{A4:volume up:}$(panel_volicon)%{A}%{A}%{A}%{A}> "/tmp/panel-fifo1"
-[ -e /tmp/panel-fifo ] && echo "V"%{A:dvol:}%{A2:volume mute:}%{A5:volume down:}%{A4:volume up:}$(panel_volicon)%{A}%{A}%{A}%{A}> "/tmp/panel-fifo"
+echo "V"%{A:dvol:}%{A2:volume mute:}%{A5:volume down:}%{A4:volume up:}$(panel_volicon)%{A}%{A}%{A}%{A}> "/tmp/panel-fifo1"
