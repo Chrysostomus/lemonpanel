b7bce4a680d14cd4e5a6a5c545089c7841630f86
Chrysostomus <mattitheman@gmail.com>
2016-01-15 22:53:14 +0700

    Update volume_status.sh

--- bin/volume_status.sh
+++ bin/volume_status.sh
@@ -5,10 +5,28 @@ panel_volicon()
         volLevel=$(pulseaudio-ctl C)
 
         if [ "$volStatus" = "yes" ]
-                then echo $GREEN " ${volLevel}"
+                then echo " ${volLevel}"
         elif [ "$volStatus" = "no" ]
-                then echo $RED " ${volLevel}"
-	else echo " ?"
+                then echo " ${volLevel}"
+		else echo " ?"
         fi
 }
-echo "V"%{A:dvol:}%{A2:volume mute:}%{A5:volume down 1:}%{A4:volume up 1:}$(panel_volicon)%{A}%{A}%{A}%{A}> "/tmp/panel-fifo1"
+
+panel_avolicon()
+{
+        avolStatus=$(avol state)
+        avolLevel=$(avol level)
+
+        if [ "$avolStatus" = "off" ]
+                then echo " ${avolLevel}"
+        elif [ "$avolStatus" = "on" ]
+                then echo " ${avolLevel}"
+		else echo " ?"
+        fi
+}
+
+if [ $(pgrep pulseaudio >/dev/null) ]; then
+		echo "V"%{A:dvol:}%{A2:volume mute:}%{A5:volume down 1:}%{A4:volume up 1:}$(panel_volicon)%{A}%{A}%{A}%{A}> "/tmp/panel-fifo1" 
+	else
+		echo "V"%{A:dvol:}%{A2:volume mute:}%{A5:volume down 1:}%{A4:volume up 1:}$(panel_avolicon)%{A}%{A}%{A}%{A}> "/tmp/panel-fifo1"
+fi
