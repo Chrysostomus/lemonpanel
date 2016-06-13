934c1aa5ba819e64ea7dc66acec2455a53b0924c
Chrysostomus <mattitheman@gmail.com>
2016-06-13 12:59:00 +0300

    Update networkmenu.sh

--- bin/networkmenu.sh
+++ bin/networkmenu.sh
@@ -1,18 +1,21 @@
-#!/bin/dash
+#!/bin/bash
+
+. ~/.profile
+[[ $TERMINAL == /usr/bin/roxterm ]] && bspc rule -a Roxterm state=pseudo_tiled -o
 
 if systemctl status systemd-networkd.service | grep -q " active"; then
 	if which wpa_gui >/dev/null 2>&1 ; then 
 		wpa_gui
 	else
-		smartsplit ; default-terminal -e wpa_tui	
+		smartsplit ; default-terminal --geometry=450x200 -e wpa_tui	
 	fi
 	 
-else 
-	if systemctl status Networkmanager.service | grep -q " active"; then
+elif  systemctl status Networkmanager.service | grep -q " active"; then
 		if which nmcli_dmenu >/dev/null 2>&1 ; then
 			networkmenuplacer.sh ; nmcli_dmenu
 		else 
 			smartsplit ; default-terminal -e nmtui
 		fi
-	fi
+else
+	smartsplit ; default-terminal --geometry=450x200 -e wpa_tui
 fi
